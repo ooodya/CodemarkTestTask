ad7582a5184ca8ade8059ac6774a5a5567103867
Vadim <Vadim@DESKTOP-I0DPT0C>
2020-10-20 14:47:17 +0300

    Project creation

---
+++ src/main/java/com/zaytsev/codemarkTestTask/CodemarkTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zaytsev.codemarkTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemarkTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodemarkTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaytsev/codemarkTestTask/CodemarkTestTaskApplicationTests.java
@@ -0,0 +1,15 @@
+package com.zaytsev.codemarkTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodemarkTestTaskApplicationTests 
+{
+
+	@Test
+	void contextLoads() 
+	{
+	}
+
+}
